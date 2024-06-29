dc43c0132397423e79b74439b946163757c7cc2f
harsh <hrmauny@ncsu.edu>
2024-06-28 21:21:11 -0400

    api key removed

--- spotify/credential.py
+++ spotify/credential.py
@@ -1,4 +1,4 @@
-CLIENT_ID="acead707e0b7415b814f58a82ee4b1b5"
-CLIENT_SECRET="8f6362a073cb4db2ba39c2500163acce"
+CLIENT_ID=
+CLIENT_SECRET=
 REDIRECT_URI="http://127.0.0.1:8000/spotify/redirect/"
 BASE_URL="https://api.spotify.com/v1/me/"
